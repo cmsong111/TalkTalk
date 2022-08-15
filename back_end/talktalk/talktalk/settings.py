e2b55de2900eb8ac47b48a443d4fb0e9c5933e84
Namju Kim <cmsong111@naver.com>
2022-08-15 15:04:34 +0900

    setting .env , setting mariaDB

--- back_end/talktalk/talktalk/settings.py
+++ back_end/talktalk/talktalk/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tcd=alj1b0kt2bd!n-^0i5t+8e1wsuq7@uy2k4v)5w0yb%mf^l'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'talktalk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
+        'NAME': 'talktalk',
+        'USER': 'root',
+        'PASSWORD': os.environ.get('DATABASES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
